66bdd2f36edcf26407dc9d801e8283c169e1050f
4EverStudent <mail@4everstudent.com>
2016-09-27 15:48:12 +0100

    Add Play and Pause buttons and functions.

--- app/src/main/java/a4everstudent/sounddemo/MainActivity.java
+++ app/src/main/java/a4everstudent/sounddemo/MainActivity.java
@@ -1,18 +1,33 @@
 package a4everstudent.sounddemo;
 
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mplayer;
+
+    public void playAudio(View view){
+
+        mplayer.start();
+
+    }
+
+    public void pauseAudio(View view){
+
+        mplayer.pause();
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MediaPlayer mPlayer = MediaPlayer.create(this, R.raw.looperman_slapjohnson);
+        mplayer = MediaPlayer.create(this, R.raw.looperman_slapjohnson);
 
-        mPlayer.start();
+
+
     }
 }
